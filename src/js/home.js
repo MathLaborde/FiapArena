4fabe0b39619dad957eda02af81cc1cc700d510b
Matheus Laborde <mlabordebr@gmail.com>
2024-08-01 23:00:13 -0300

    Implementação de header e footer em todas as páginas

--- src/js/home.js
+++ src/js/home.js
@@ -37,3 +37,14 @@ document.addEventListener("DOMContentLoaded", function() {
         // Adicione a funcionalidade desejada aqui
     });
 });
+
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const bars = document.querySelectorAll('.menu-hamburger .bar');
+  
+    menu.classList.toggle('show-menu');
+  
+    bars[0].classList.toggle('rotate1');
+    bars[1].classList.toggle('rotate2');
+    bars[2].classList.toggle('rotate3');
+}
--- src/js/ranking.js
+++ src/js/ranking.js
@@ -1,10 +1,10 @@
-function mostrarMenu() {
-    let menuMobile = document.querySelector('.mobile-menu')
-    if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open')
-        document.querySelector('.icon').src = "./src/img/menu_white_36dp.svg"
-    } else {
-        menuMobile.classList.add('open')
-        document.querySelector('.icon').src = "./src/img/close_white_36dp.svg"
-    }
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const bars = document.querySelectorAll('.menu-hamburger .bar');
+  
+    menu.classList.toggle('show-menu');
+  
+    bars[0].classList.toggle('rotate1');
+    bars[1].classList.toggle('rotate2');
+    bars[2].classList.toggle('rotate3');
 }
