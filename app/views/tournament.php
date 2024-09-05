ac71b35938d262ca2df6142974f356d9aacf5611
Viilang <83655188+Viilang@users.noreply.github.com>
2024-09-05 02:57:50 -0300

    Add files via upload
    ajustei as chamadas dos dados do banco no tournament.php

--- app/views/tournament.php
+++ app/views/tournament.php
@@ -1,165 +1,43 @@
 <link rel="stylesheet" href="/public/src/css/tournament.css" />
 <div class="main-content">
   <div class="image-background">
+  <div class="text-overlay">
+<!-- Inseção de dados com php -->
+<h1 class="main-title"><?php echo $tournament['name'] ?></h1>
+<div class="align-arrow">
+        <div class="down-arrow"></div>
+  </div>
+<p class="main-description"><?php echo $tournament['description'] ?></p>
+
+  </div>
 
     <!-- <img src="/public/src/img/fundo.jpg" alt="plano de fundo" /> -->
 
     <!-- Exemplo de imagem base64, basicamente vc tem que adicionar data:image/jpeg;base64, antes de imprimir o valor do base64-->
     <img src="data:image/jpeg;base64,<?php echo htmlspecialchars($tournament['image']); ?>" alt="plano de fundo" />
-
-  </div>
-  <div class="text-overlay">
-
-    <!-- Exemplo de inseção de dados com php -->
-    <h1 class="main-title"><?php echo $tournament['name'] ?></h1>
-
-
-    <h2 class="main-description">
-      Esse torneio é voltado para estudantes da FIAP de níveis Prata e Ouro.
-      Os participantes terão a chance de mostrar suas habilidades e competir
-      por um prêmio exclusivo: um passe de skins raras. Junte-se a nós para
-      esta batalha épica e prove que você é o melhor campeão da FIAP!
-    </h2>
   </div>
 </div>
-
-<div class="down-arrow"></div>
 
 <div class="bottons">
-  <div id="show-or-hide" class="btn btn1">Ver regras</div>
-  <div id="show-or-hide" class="btn btn2">Ver Participantes</div>
-  <div id="show-or-hide" class="btn btn3">Ver premiação</div>
+  <div id="show-rules" class="btn btn1">Ver regras</div>
+  <div id="show-participants" class="btn btn2">Ver Participantes</div>
+  <div id="show-award" class="btn btn3">Ver premiação</div>
 </div>
 
-<section class="container">
-  <div class="teams-content">
-    <div class="team1">
-      <h2 class="titulo-team">Time 1</h2>
-      <ul class="team-list">
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Joao"
-              class="profile-pic" />
-            João
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Maria"
-              class="profile-pic" />
-            Maria
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Gabriel"
-              class="profile-pic" />
-            Gabriel
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Joquina"
-              class="profile-pic" />
-            Joquina
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Fernanda"
-              class="profile-pic" />
-            Fernanda
-          </a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="chat-box">
-      <div class="chat-header">Chat</div>
-      <div class="chat-content">
-        <div class="message"><span class="user">João:</span> Olá!</div>
-        <div class="message">
-          <span class="user">Maria:</span> Tudo bem?
-        </div>
-        <div class="digita">
-          <span class="user digitando">João está digitando..</span>
-        </div>
-      </div>
-      <div class="chat-input">
-        <input type="text" id="chat" placeholder="Digite sua mensagem..." />
-        <button id="enviar-chat">Enviar</button>
-      </div>
-    </div>
-
-    <div class="team2">
-      <h2 class="titulo-team">Time 2</h2>
-      <ul class="team-list">
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Joao"
-              class="profile-pic" />
-            Pato Donald
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Maria"
-              class="profile-pic" />
-            Mickey
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Gabriel"
-              class="profile-pic" />
-            Thor
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Joquina"
-              class="profile-pic" />
-            Wolverine
-          </a>
-        </li>
-        <li>
-          <a href="#" class="profile-link">
-            <img
-              src="/public/src/img/profile.jpg"
-              alt="Fernanda"
-              class="profile-pic" />
-            Spiderman
-          </a>
-        </li>
-      </ul>
-    </div>
+<div class="informations">
+  <div id="box-teamsaward" class="box-teamsaward">
+    <h1 class="teams-award"><?php echo $tournament['award'] ?></h1>
   </div>
 
-  <div class="teams-award">
-    O prêmio será um novo pacote de Skins para a personagem Soraka
+  <div id="box-teamsrules" class="box-teamsrules">
+    <h1 class="teams-award"><?php echo $tournament['rules'] ?></h1>
   </div>
 
-  <div class="teams-rules">
-    Proibição de Magia: Personagens mágicos não são permitidos durante a
-    partida.
-  </div>
-</section>
+  <div id="box-teams-createby" class="box-teams-createby">
+  <a href="https://www.fiap.com.br/" class="invite-button">Convidar amigos</a>
+</div>
+
+</div>
+
+
 <script src="/public/src/js/tournament.js"></script>
